78c0c1eff0b32fece57a2f7a47c83148027a4c1b
Juan Camilo Castro <130163554+Tercmo@users.noreply.github.com>
2023-07-11 15:53:58 -0500

    Implementación de la API básica. Home Funcional.

--- src/index.js
+++ src/index.js
@@ -2,24 +2,20 @@ import './css/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { Provider } from 'react-redux';
 import store from './store/store';
-
+import conf_auth from './conf_auth';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
    <Provider store={store}>
-    <Auth0Provider 
-     domain='tecrmoprueba.us.auth0.com'
-     clientId='vsHBtvqA6StHm0gYvA4Zl4KLKa6T08aG'
-     redirectUri={window.location.origin} >
+    <Auth0Provider {...conf_auth} >
     <App />
     </Auth0Provider>
     </Provider>
   </React.StrictMode>
 );
 
-reportWebVitals();
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,42 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import React from 'react'
+import React, { Fragment } from 'react'
 import Axios from 'axios'
 import { useEffect, useState } from 'react';
 
 const Home = () => {
-    const { user, isAuthenticated, isLoading } = useAuth0();
-    const key = 'AsQys6MsjhjNVkymKoszaQ==GLZFjEfqtVfSTTkD';
+    const { user } = useAuth0();
+    const [celebrity, setCelebrity] = useState([]);
+    const key = process.env.REACT_APP_KEY;
     const headers = {'X-Api-Key': key };
    const nombre = 'Michael Jordan';
-    const url ='https://api.api-ninjas.com/v1/celebrity?name='+nombre;
+    const url = process.env.REACT_APP_URL_APP+nombre;
     useEffect( ()=>{
       Axios.get(url, {headers})
-      .then(resp=>{console.log(resp.data)})
+      .then(resp=>{console.log(resp.data)
+      setCelebrity(resp.data)
+      console.log(celebrity)
+      })
       .catch(error=>{console.log(error)})
     },[]
     
     )
    
-   /* if (isLoading) {
-      return <div>Cargando...</div>;
-    }
-  
-    return (
-      isAuthenticated && (
-        <div className='bs-autCard'>
-        <img src={user.picture} className='bs-autCard-image' alt={user.name}  />
-        <h3 className='bs-autCard-name'>{user.name} </h3>
-        <p className='bs-autCard-email'> Correo Electronico: {user.email}</p>
-           </div>
-      )
-    );*/
 
 return (
-  <div> Home </div>
+  <div className='bs-autCard'>
+  <img src={user.picture} className='bs-autCard-image' alt={user.name}  />
+  <h3 className='bs-autCard-name'>{user.name} </h3>
+  <p className='bs-autCard-email'> Correo Electronico: {user.email}</p> 
+  <div>
+
+  </div>
+  <Fragment>
+  {celebrity.map(cel=>{return(
+    <h4> {cel.name} - {cel.birthday} - {cel.deathday} - {cel.place_of_birth} - {cel.biography} - {cel.gender} - {cel.imdb_}
+    </h4>
+    
+    )})}</Fragment>
+  </div>
 )
 
 }
