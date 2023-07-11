d12b856704fcec8de1f1f1806c9b351370c75650
Juan Camilo Castro <130163554+Tercmo@users.noreply.github.com>
2023-07-10 22:42:14 -0500

    Se modifica Index, Login se introducen las acciones

--- src/index.js
+++ src/index.js
@@ -4,16 +4,21 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { Provider } from 'react-redux';
+import store from './store/store';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+   <Provider store={store}>
     <Auth0Provider 
      domain='tecrmoprueba.us.auth0.com'
      clientId='vsHBtvqA6StHm0gYvA4Zl4KLKa6T08aG'
      redirectUri={window.location.origin} >
     <App />
     </Auth0Provider>
+    </Provider>
   </React.StrictMode>
 );
 
