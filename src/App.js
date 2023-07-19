ccf8df38c7863ed9516df5477575b24a5ef4daa2
Juan Camilo Castro <130163554+Tercmo@users.noreply.github.com>
2023-07-18 19:50:37 -0500

    cambio en App

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import {Login} from './auth/Login';
-import { Logout} from './auth/Logout';
-import { Link,Route,Routes } from 'react-router-dom';
+import { Login } from './auth/Login';
+import { Logout } from './auth/Logout';
+import { Link, Route, Routes } from 'react-router-dom';
 import { Favorites } from './pages/Favorites';
 import SearchBar from './components/SearchBar';
 
 
 function App() {
-  const {isAuthenticated} =useAuth0();
+  const { isAuthenticated } = useAuth0();
   return (
     <div className="App">
       <header className="App-header">
-         {isAuthenticated ? (
+        {isAuthenticated ? (
           <>
-          <nav> 
-         
-         <Link to='/home' >Portada </Link>
-         <Link to='/favorites' >Favoritos </Link>
-         <Link to='/logout' >Cerrar Sesión</Link>
-          </nav>
+            <nav>  
+              <Link to='/home' >Portada </Link>
+              <Link to='/favorites' >Favoritos </Link>
+              <Link to='/logout' >Cerrar Sesión</Link>
+            </nav>
           </>
-         ): (
-          <Login/>
-         )}  
+        ) : (
+          <Login />
+        )}
       </header>
-  
+
       <Routes>
         <Route path='/favorites' element={<Favorites />} />
         <Route path='/home' element={<SearchBar />} />
