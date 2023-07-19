f7b673683279e9e3526d7e72e4852f599aa0cafd
Juan Camilo Castro <130163554+Tercmo@users.noreply.github.com>
2023-07-19 08:46:05 -0500

    Se agregó modo claro y modo oscuro se puso el botón de login en la parte superior Se agrega css

--- src/App.js
+++ src/App.js
@@ -1,34 +1,61 @@
+import React, { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Login } from './auth/Login';
-import { Logout } from './auth/Logout';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes, Navigate } from 'react-router-dom';
 import { Favorites } from './pages/Favorites';
 import SearchBar from './components/SearchBar';
-
+import { Login } from './auth/Login';
+import { Logout } from './auth/Logout';
+import './css/general.css';
 
 function App() {
   const { isAuthenticated } = useAuth0();
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
+
+
+  document.body.className = isDarkMode ? 'dark-mode' : 'light-mode';
+
   return (
     <div className="App">
-      <header className="App-header">
-        {isAuthenticated ? (
-          <>
-            <nav>  
-              <Link to='/home' >Portada </Link>
-              <Link to='/favorites' >Favoritos </Link>
-              <Link to='/logout' >Cerrar Sesión</Link>
-            </nav>
-          </>
-        ) : (
-          <Login />
-        )}
+      <header>
+        <nav>
+          <Link to='/home'>Portada</Link>
+          {isAuthenticated && <Link to='/favorites'>Favoritos</Link>}
+          {isAuthenticated ? (
+            <Link to='/logout'>Cerrar Sesión</Link>
+          ) : (
+            <Login />
+          )}
+          <button
+            className={`mode-button ${isDarkMode ? 'dark-mode' : 'light-mode'}`}
+            onClick={toggleDarkMode}
+          >
+            {isDarkMode ? 'Modo Oscuro' : 'Modo Claro'}
+          </button>
+        </nav>
       </header>
 
-      <Routes>
-        <Route path='/favorites' element={<Favorites />} />
-        <Route path='/home' element={<SearchBar />} />
-        <Route path='/logout' element={<Logout />} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route
+            path='/favorites'
+            element={isAuthenticated ? <Favorites /> : <Navigate to='/login' />}
+          />
+          <Route
+            path='/home'
+            element={isAuthenticated ? <SearchBar /> : <Navigate to='/login' />}
+          />
+          <Route
+            path='/logout'
+            element={isAuthenticated ? <Logout /> : <Navigate to='/login' />}
+          />
+        </Routes>
+      </main>
+
+      <footer>{/* si alcanzo a hacer el pie de página */}</footer>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,7 @@
+import React, { useState, useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import React from 'react'
-import Axios from 'axios'
-import { useEffect, useState } from 'react';
+import Axios from 'axios';
 import CelebritiesList from '../components/CelebritiesList';
-
 
 const Home = () => {
   const { user } = useAuth0();
@@ -12,28 +10,26 @@ const Home = () => {
   const key = process.env.REACT_APP_KEY;
   const headers = {
     'X-Api-Key': key
-  }
- 
+  };
+
   const url = process.env.REACT_APP_URL_APP;
   useEffect(() => {
     Axios.get(url, { headers })
       .then(resp => {
         setCelebrity(resp.data);
-        
-      })
-  }, [] )
+      });
+  }, []);
 
-  return (   
+  return (
+    <div>
+      <img className='profile-img' src={user?.picture} alt={user?.name} />
+      <h3>{user?.name}</h3>
+      <p>Correo Electronico: {user?.email}</p>
 
-  <div>
-  <img className='profile-img' src={user.picture} alt={user.name}  />
-  <h3>{user.name} </h3>
-  <p> Correo Electronico: {user.email}</p> 
-  
-  <CelebritiesList celebrities={celebrity}/>
-  
-  </div>
-  
-)
-}
-export default Home
+      <CelebritiesList celebrities={celebrity} />
+
+    </div>
+  );
+};
+
+export default Home;
