e4326ae5732186291e9445edaf4e8a6ee6d765f1
Juan Camilo Castro <130163554+Tercmo@users.noreply.github.com>
2023-07-19 18:42:10 -0500

    Primera Aplicación de Boostrap

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,38 +1,27 @@
-import React, { Fragment } from 'react'
-import {useSelector } from 'react-redux'
-import '../css/general.css'; 
+import React, { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import '../css/general.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 export const Favorites = () => {
+  const { celebritiesList } = useSelector((state) => state.cel);
 
-    const { celebritiesList } = useSelector(state => state.cel);
-    return (
-        <Fragment>
-            <div>Listado de Favoritos</div>
-            <table>
-                <thead>
-                    <tr>
-                        <th scope='col'>ID </th>
-                        <th scope='col'>Nombre </th>
-                        <th scope='col'>Nacionalidad </th>
-                        <th scope='col'>Edad </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {celebritiesList.map(c => {
-                        return (
-                            <tr>
-                                <td scope='row'>{c.birthday}</td>
-                                <td scope='row'>{c.name}</td>
-                                <td scope='row'>{c.nationality}</td>
-                                <td scope='row'>{c.age}</td>
-                            </tr>
-                        )
-                    })}
-
-
-                </tbody>
-            </table>
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <div>Listado de Favoritos</div>
+      <div className="row row-cols-lg-4 row-cols-md-3 row-cols-sm-1 g-2">
+        {celebritiesList.map((c) => (
+          <div className="col" key={c.birthday}>
+            <div className="card">
+              <div className="card-body">
+                <h4 className="card-title">{c.name}</h4>
+                <h6 className="card-subtitle mb-2 text-muted">{c.nationality}</h6>
+                <p className="card-text">Edad: {c.age}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </Fragment>
+  );
+};
