a184ae0dcb4d2cac33d5ae95e4da4190df64518c
Juan Camilo Castro <130163554+Tercmo@users.noreply.github.com>
2023-07-19 20:14:38 -0500

    Se unifica tamaño de todas las card se da margenes para responsive

--- src/components/CelebritiesList.js
+++ src/components/CelebritiesList.js
@@ -20,12 +20,12 @@ const CelebritiesList = ({ celebrities }) => {
       <div className="row row-cols-lg-4 row-cols-md-3 row-cols-sm-1 g-2">
         {celebrities.map((c, index) => (
           <div className="col" key={index}>
-            <div className="card">
+            <div className="card h-100 d-flex align-items-center justify-content-center">
               <div className="card-body">
                 <h2 className="card-title">{c.name}</h2>
                 <h4 className="card-subtitle mb-2 text-body-secondary">{c.age}</h4>
-                <h4 className="card-subtitle mb-2 text-body-secondary"> {c.gender}</h4>
-                
+                <h4 className="card-subtitle mb-2 text-body-secondary">{c.gender}</h4>
+
                 <button
                   type="button"
                   className="btn btn-primary"
