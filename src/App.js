f4926ec37a21ecbc9b7ad77f46b476213f0f5161
Juan Camilo Castro <130163554+Tercmo@users.noreply.github.com>
2023-07-20 14:22:46 -0500

    Inclusión de logo

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ import { Login } from './auth/Login';
 import { Logout } from './auth/Logout';
 import './css/general.css';
 
+import logoImage from './images/Tera.png'; // Ruta a tu imagen logo.png
+
 function App() {
   const { isAuthenticated, user } = useAuth0();
   const [isDarkMode, setIsDarkMode] = useState(() => localStorage.getItem('isDarkMode') === 'true');
+  const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
     localStorage.setItem('isDarkMode', isDarkMode);
@@ -17,17 +20,47 @@ function App() {
   }, [isDarkMode]);
 
   const toggleDarkMode = () => {
-    setIsDarkMode((prevMode) => !prevMode);
+    setIsDarkMode(prevMode => !prevMode);
   };
 
-  const [favorites, setFavorites] = useState([]);
+  const renderUserInfo = () => (
+    <div className="ml-lg-auto d-flex align-items-center user-actions">
+      <div className="user-info d-flex align-items-center">
+        <img
+          className="profile-img rounded-circle"
+          src={user?.picture}
+          alt={user?.name}
+          style={{ width: '40px', height: '40px', marginRight: '8px' }}
+        />
+        <div style={{ lineHeight: '6%' }}>
+          <h5 className="text-dark">{user?.name}</h5>
+          <p className="text-dark">{user?.email}</p>
+        </div>
+      </div>
+    </div>
+  );
+
+  const renderMenuLinks = () => (
+    <>
+      <Link to="/home" className="nav-link active" aria-current="page">
+        Inicio
+      </Link>
+      <Link to="/favorites" className="nav-link">
+        Favoritos
+      </Link>
+    </>
+  );
 
   return (
-    <div className="App">
+    <div className={`App ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
       <nav className={`navbar navbar-expand-lg navbar-light bg-light ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
         <div className="container">
-          {/* Columna 1 */}
           <Link to="/home" className="navbar-brand">
+            <img
+              src={logoImage}
+              alt="Logo de Proyecto Tera"
+              style={{ width: '38px', height: '38px', marginRight: '8px' }}
+            />
             Proyecto Tera
           </Link>
 
@@ -45,41 +78,12 @@ function App() {
 
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav">
-              {isAuthenticated && (
-                <>
-                  <Link to="/home" className="nav-link active" aria-current="page">
-                    Inicio
-                  </Link>
-                  <Link to="/favorites" className="nav-link">
-                    Favoritos
-                  </Link>
-                </>
-              )}
+              {isAuthenticated && renderMenuLinks()}
             </div>
-            {isAuthenticated ? (
-              <div className="ml-lg-auto d-flex align-items-center user-actions">
-                <div className="user-info d-flex align-items-center">
-                  <img
-                    className="profile-img rounded-circle"
-                    src={user?.picture}
-                    alt={user?.name}
-                    style={{ width: '40px', height: '40px', marginRight: '8px' }}
-                  />
-                  <div style={{ lineHeight: '6%' }}>
-                    <h5 className="text-dark">{user?.name}</h5>
-                    <p className="text-dark">{user?.email}</p>
-                  </div>
-                </div>
-              </div>
-            ) : (
-              <Login />
-            )}
+            {isAuthenticated ? renderUserInfo() : <Login />}
           </div>
           {isAuthenticated && (
-            <button
-              className={`mode-button ${isDarkMode ? 'dark-mode' : 'light-mode'}`}
-              onClick={toggleDarkMode}
-            >
+            <button className={`mode-button ${isDarkMode ? 'dark-mode' : 'light-mode'}`} onClick={toggleDarkMode}>
               {isDarkMode ? 'Modo Oscuro' : 'Modo Claro'}
             </button>
           )}
@@ -89,10 +93,7 @@ function App() {
 
       <main>
         <Routes>
-          <Route
-            path="/favorites"
-            element={isAuthenticated ? <Favorites /> : <Navigate to="/login" />}
-          />
+          <Route path="/favorites" element={isAuthenticated ? <Favorites /> : <Navigate to="/login" />} />
           <Route
             path="/home"
             element={isAuthenticated ? <SearchBar favorites={favorites} setFavorites={setFavorites} /> : <Navigate to="/login" />}
